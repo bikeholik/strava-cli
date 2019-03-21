9a8b9be3d392c5236918f04318a1ae745fb8eba2
bikeholik <bikeholik@gmail.com>
2019-03-21 12:51:23 +0000

    Range of dates

--- src/main/kotlin/com/github/bikeholik/stravacli/CloneActivityCommand.kt
+++ src/main/kotlin/com/github/bikeholik/stravacli/CloneActivityCommand.kt
@@ -6,11 +6,14 @@ import com.github.ajalt.clikt.parameters.options.multiple
 import com.github.ajalt.clikt.parameters.options.option
 import com.github.ajalt.clikt.parameters.options.required
 import com.github.ajalt.clikt.parameters.types.long
+import io.swagger.client.ApiException
 import io.swagger.client.api.ActivitiesApi
 import org.springframework.stereotype.Component
 import org.threeten.bp.OffsetDateTime
 import org.threeten.bp.temporal.Temporal
 import java.time.LocalDate
+import java.time.Period
+import java.util.stream.LongStream
 import java.util.stream.Stream
 
 fun Boolean.toInt() = if (this) 1 else 0
@@ -20,27 +23,43 @@ class CloneActivityCommand(val activitiesApi: ActivitiesApi) : CliktCommand(name
     val log = logger()
     val activityId by option("--activity-id").long().required()
     val dates by option("--date").convert { it -> LocalDate.parse(it) }.multiple()
+    val from by option("--from").convert { it -> LocalDate.parse(it) }
+    val to by option("--to").convert { it -> LocalDate.parse(it) }
     override fun run() {
-        log.info("Get activity {}", activityId)
-//        log.info("Activities: {}", activitiesApi.getLoggedInAthleteActivities(null, null, null, null))
-        val activity = activitiesApi.getActivityById(activityId, false)
-        log.info("Activity to clone: {}", activity)
-        val starts = if (dates.isEmpty()) Stream.of(LocalDate.now()) else dates.stream()
-        starts
-                .map { d -> activity.startDateLocal.with { it -> adjustWith(it as OffsetDateTime, d) } }
-                .peek { d -> log.info("Cloning at {}", d) }
-                .forEach { d ->
-                    activitiesApi.createActivity(
-                            activity.name,
-                            activity.type.name,
-                            d.toString(),
-                            activity.elapsedTime,
-                            activity.description + "\n--cloned from ${activity.id}",
-                            activity.distance,
-                            activity.isTrainer.toInt(),
-                            "",
-                            activity.isCommute.toInt())
-                }
+        log.debug("Get activity {}", activityId)
+        try {
+            val activity = activitiesApi.getActivityById(activityId, false)
+            log.debug("Activity to clone: {}", activity)
+            dates()
+                    .map { d -> activity.startDateLocal.with { it -> adjustWith(it as OffsetDateTime, d) } }
+                    .peek { d -> log.debug("Cloning at {}", d) }
+                    .forEach { d ->
+                        activitiesApi.createActivity(
+                                activity.name,
+                                activity.type.name,
+                                d.toString(),
+                                activity.elapsedTime,
+                                activity.description + "\n--cloned from ${activity.id}",
+                                activity.distance,
+                                activity.isTrainer.toInt(),
+                                "",
+                                activity.isCommute.toInt())
+                    }
+        } catch (e: ApiException) {
+            log.error("Failure cloning activity {}", activityId, e)
+        }
+    }
+
+    private fun dates(): Stream<LocalDate> {
+        if (dates.isEmpty() && (from == null || to == null || from?.isAfter(to) == true)) {
+            return Stream.of(LocalDate.now())
+        } else if (!dates.isEmpty()) {
+            return dates.stream()
+        } else {
+            return LongStream.range(0, Period.between(from, to).days.toLong())
+                    .mapToObj { i -> from?.plusDays(i) }
+
+        }
     }
 
     private fun adjustWith(it: OffsetDateTime, date: LocalDate): Temporal? {
